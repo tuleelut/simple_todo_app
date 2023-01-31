3814cdd18606e3ec4d064cdeb99aa08a69a6ebcd
Rita Daukšienė <rita78@gmail.com>
2023-01-31 23:36:09 +0200

    todos search implemented

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 "use strict";
 let url = "https://63d5107ac52305feff6d6315.mockapi.io/todos/";
 let reload = () => location.reload();
-let error = (error) => console.log("Uuuups 💥klaida");
+let error = (error) => console.log(error.message);
 let editing = false;
 let editingPostId = null;
+const searchField = document.querySelector(".search input");
+const todosList = document.querySelector(".todos");
 
 //ToDo list atvaizdavimas html READ
 window.addEventListener("load", (event) => {
@@ -19,16 +21,42 @@ window.addEventListener("load", (event) => {
     .catch(error);
 });
 
-//TODo pridėjimas
+//ToDo pridėjimas
 document.querySelector(".add").addEventListener("submit", (event) => {
   event.preventDefault();
   createTodo();
   event.target.reset();
 });
 
+//ToDo paieška
+searchField.addEventListener("focus", (e) => {
+  // e.preventDefault();
+  fetch(url)
+    .then((response) => response.json())
+    .then((todos) => {
+      searchField.addEventListener("input", () => {
+        todosList.innerHTML = "";
+        let searchString = searchField.value.trim();
+        
+        let searchResults = todos.filter((element) =>
+          element.todo.toLowerCase().includes(searchString.toLowerCase())
+        );
+
+        if (searchResults.length == 0) {
+          todosList.innerHTML = "Nieko neradome";
+        } else {
+          searchResults.forEach((element) => {
+            showTodo(element);
+          });
+        }
+      });
+    })
+    .catch(error);
+});
+
 // funkcija atvaizdavimui ir delete mygtukui
 let showTodo = (el) => {
-  document.querySelector(".todos").innerHTML += `
+  todosList.innerHTML += `
     <li class="list-group-item d-flex justify-content-between align-items-center">
     <span>${el.todo}</span>
     <span>${el.createdAt.slice(0, 10)}</span>
@@ -50,25 +78,12 @@ let createTodo = () => {
       createdAt: new Date(),
       todo: todoTitle,
     };
+
     if (editing) {
       //   console.log(editing, editingPostId);
-      const requestOptions = {
-        method: "PUT",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(todo),
-      };
-      fetch(url + editingPostId, requestOptions).then(reload);
-      editing = false;
-
+      putTodo(todo);
     } else {
-      const requestOptions = {
-        method: "POST",
-        body: JSON.stringify(todo),
-        headers: {
-          "Content-type": "application/json; charset=UTF-8",
-        },
-      };
-      fetch(url, requestOptions).then(reload).catch(error);
+      postTodo(todo);
     }
   }
 };
@@ -90,3 +105,26 @@ let editTodo = (title, id) => {
   editingPostId = id;
   //   console.log(title, id, editing);
 };
+
+//Update TODO via PUT
+function putTodo(todo) {
+  const requestOptions = {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(todo),
+  };
+  fetch(url + editingPostId, requestOptions).then(reload);
+  editing = false;
+}
+
+//Add new TODO via POST
+function postTodo(todo) {
+  const requestOptions = {
+    method: "POST",
+    body: JSON.stringify(todo),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  };
+  fetch(url, requestOptions).then(reload).catch(error);
+}
